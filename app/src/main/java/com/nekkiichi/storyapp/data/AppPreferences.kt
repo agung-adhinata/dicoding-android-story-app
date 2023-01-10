fc9c2e65a565df8dc10942a0589f4a95e9182fb5
Agung Adhinata <adhi0asta@gmail.com>
2023-01-10 23:38:04 +0800

    add getUserName

--- app/src/main/java/com/nekkiichi/storyapp/data/AppPreferences.kt
+++ app/src/main/java/com/nekkiichi/storyapp/data/AppPreferences.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class AppPreferences constructor(private val dataStore: DataStore<Preferences>) {
-
     suspend fun setLoginResponse(tokenKey: String, username: String, userId: String) {
         dataStore.edit {
             it[TOKEN_KEY] = tokenKey
@@ -16,7 +15,6 @@ class AppPreferences constructor(private val dataStore: DataStore<Preferences>) 
             it[USER_ID] = userId
         }
     }
-
     suspend fun clearLoginStatus() {
         dataStore.edit {
             it[TOKEN_KEY] = ""
@@ -34,8 +32,11 @@ class AppPreferences constructor(private val dataStore: DataStore<Preferences>) 
             it[USER_ID]
         }
     }
-
-
+    fun getUserName(): Flow<String?> {
+        return dataStore.data.map {
+            it[USER_NAME]
+        }
+    }
     companion object {
         val TOKEN_KEY = stringPreferencesKey("auth_token_key")
         val USER_NAME = stringPreferencesKey("auth_user_name")
