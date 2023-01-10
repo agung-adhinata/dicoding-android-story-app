48231de429f8bae93e03242c19a73af92920b1e6
Agung Adhinata <adhi0asta@gmail.com>
2023-01-10 23:38:22 +0800

    create repository

---
+++ app/src/main/java/com/nekkiichi/storyapp/data/StoryRepository.kt
@@ -0,0 +1,68 @@
+package com.nekkiichi.storyapp.data
+
+import android.util.Log
+import com.nekkiichi.storyapp.data.remote.response.BasicResponse
+import com.nekkiichi.storyapp.data.remote.response.FullAuthResponse
+import com.nekkiichi.storyapp.data.remote.response.ListStoryItemResponse
+import com.nekkiichi.storyapp.data.remote.response.ListStoryResponse
+import com.nekkiichi.storyapp.data.remote.services.ApiService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class StoryRepository @Inject constructor(
+    private val apiService: ApiService,
+    private val preferences: AppPreferences
+) {
+    fun requestLogin(email: String, password: String):Flow<ResponseStatus<FullAuthResponse>> = flow {
+        emit(ResponseStatus.loading)
+        try {
+            val res = apiService.sendLogin(email,password)
+            emit(ResponseStatus.Success(res))
+            if(res.error == false) {
+                emit(ResponseStatus.Success(res))
+            }else {
+                Log.d(TAG, "Server Error, ${res.message}")
+                emit(ResponseStatus.Error(res.message.toString()))
+            }
+        }catch (e:Exception) {
+            Log.d(TAG, "error when request login, ${e.message}")
+            emit(ResponseStatus.Error(e.message.toString()))
+        }
+    }
+    fun requestRegister(name: String, email: String, password: String) : Flow<ResponseStatus<BasicResponse>> = flow {
+        emit(ResponseStatus.loading)
+        try {
+            val res = apiService.sendRegister(name, email, password)
+            if(res.error == false) {
+                emit(ResponseStatus.Success(res))
+            }else {
+                Log.d(TAG, "Server Error, ${res.message}")
+                emit(ResponseStatus.Error(res.message.toString()))
+            }
+        }catch (e: Exception) {
+            Log.d(TAG, "Error when request register, ${e.message}")
+            emit(ResponseStatus.Error(e.message.toString()))
+        }
+    }
+    fun getAllStories(): Flow<ResponseStatus<ListStoryResponse>> = flow {
+        val token ="Bearer ${preferences.getTokenkey()}"
+        emit(ResponseStatus.loading)
+        try {
+            val res = apiService.getAllStories(token,10,10)
+            if(res.error == false) {
+                emit(ResponseStatus.Success(res))
+            }else {
+                Log.d(TAG, "Server Error, ${res.message}")
+                emit(ResponseStatus.Error(res.message.toString()))
+            }
+        }catch (e: Exception) {
+            Log.d(TAG, "Error when request register, ${e.message}")
+            emit(ResponseStatus.Error(e.message.toString()))
+        }
+    }
+
+    companion object {
+        val TAG = "StoryRepository"
+    }
+}
