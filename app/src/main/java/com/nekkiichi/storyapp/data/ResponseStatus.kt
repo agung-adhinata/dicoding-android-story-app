1032a23bcb4f5ebbb1988059531515a4d7d97b79
Agung Adhinata <adhi0asta@gmail.com>
2023-01-10 23:35:20 +0800

    update response status

--- app/src/main/java/com/nekkiichi/storyapp/data/ResponseStatus.kt
+++ app/src/main/java/com/nekkiichi/storyapp/data/ResponseStatus.kt
@@ -4,4 +4,5 @@ sealed class ResponseStatus<out R> private constructor(){
     data class Success<out T>(val data: T): ResponseStatus<T>()
     data class Error(val error: String): ResponseStatus<Nothing>()
     object loading: ResponseStatus<Nothing>()
+    object init: ResponseStatus<Nothing>()
 }
