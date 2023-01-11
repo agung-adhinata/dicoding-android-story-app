6d0e9f32865ca6b4dbdfed157e0d552d52def7ba
Agung Adhinata <adhi0asta@gmail.com>
2023-01-11 21:54:46 +0800

    create data class for Api, with Serialized

---
+++ app/src/main/java/com/nekkiichi/storyapp/data/remote/request/Auth.kt
@@ -0,0 +1,6 @@
+package com.nekkiichi.storyapp.data.remote.request
+
+import com.google.gson.annotations.SerializedName
+
+data class Login(@SerializedName("email") val email: String?,@SerializedName("password")val password: String?)
+data class Register(@SerializedName("name")val name: String?,@SerializedName("email") val email: String?,@SerializedName("password")val password: String?)
