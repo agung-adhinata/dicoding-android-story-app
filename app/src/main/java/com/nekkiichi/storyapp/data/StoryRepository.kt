e20d0ea55cac332057a166c8b396e6affb770f43
Agung Adhinata <adhi0asta@gmail.com>
2023-01-15 10:33:31 +0800

    make saved token is loaded before get story

--- app/src/main/java/com/nekkiichi/storyapp/data/StoryRepository.kt
+++ app/src/main/java/com/nekkiichi/storyapp/data/StoryRepository.kt
@@ -1,12 +1,14 @@
 package com.nekkiichi.storyapp.data
 
 import android.util.Log
+import com.nekkiichi.storyapp.data.remote.request.Login
 import com.nekkiichi.storyapp.data.remote.request.Register
 import com.nekkiichi.storyapp.data.remote.response.BasicResponse
 import com.nekkiichi.storyapp.data.remote.response.FullAuthResponse
 import com.nekkiichi.storyapp.data.remote.response.ListStoryResponse
 import com.nekkiichi.storyapp.data.remote.services.ApiService
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
@@ -23,7 +25,8 @@ class StoryRepository @Inject constructor(
     fun requestLogin(email: String, password: String):Flow<ResponseStatus<FullAuthResponse>> = flow {
         emit(ResponseStatus.loading)
         try {
-            val res = apiService.sendLogin(email,password)
+            val loginJson = Login(email, password)
+            val res = apiService.sendLogin(loginJson)
             emit(ResponseStatus.Success(res))
             if(res.error == false) {
                 emit(ResponseStatus.Success(res))
@@ -53,20 +56,26 @@ class StoryRepository @Inject constructor(
         }
     }
     fun getAllStories(): Flow<ResponseStatus<ListStoryResponse>> = flow {
-        val token = "Bearer ${preferences.getToken()}"
-        emit(ResponseStatus.loading)
-        try {
-            val res = apiService.getAllStories(token, 10, 10)
-            if (res.error == false) {
-                emit(ResponseStatus.Success(res))
-            } else {
-                Log.d(TAG, "Server Error, ${res.message}")
-                emit(ResponseStatus.Error(res.message.toString()))
+        preferences.getToken().collect {
+            if(it!= null) {
+                val token = "Bearer $it"
+                emit(ResponseStatus.loading)
+                try {
+                    val res = apiService.getAllStories(token, 10, 20)
+                    if (res.error == false) {
+                        Log.d(TAG, "data: ${res.listStory.toString()}")
+                        emit(ResponseStatus.Success(res))
+                    } else {
+                        Log.d(TAG, "Server Error, ${res.message}")
+                        emit(ResponseStatus.Error(res.message.toString()))
+                    }
+                } catch (e: Exception) {
+                    Log.d(TAG, "Error when request register, ${e.message}")
+                    emit(ResponseStatus.Error(e.message.toString()))
+                }
             }
-        } catch (e: Exception) {
-            Log.d(TAG, "Error when request register, ${e.message}")
-            emit(ResponseStatus.Error(e.message.toString()))
         }
+
     }
 
     fun uploadStory(file: File, description: String): Flow<ResponseStatus<BasicResponse>> = flow {
